878b34e8de3ffd5b7b9f73d15d7b3af86bddce94
Gavin-Niederman <gavinniederman@gmail.com>
2024-06-25 14:57:45 -0700

    refactor: switch to async unix sockets

--- packages/v5d-interface/src/lib.rs
+++ packages/v5d-interface/src/lib.rs
@@ -3,6 +3,7 @@ use std::{io, path::PathBuf};
 use log::{debug, info};
 use serde::{Deserialize, Serialize};
 use socket2::{Domain, SockAddr, Socket, Type};
+use tokio::{io::{AsyncReadExt, Interest}, net::UnixStream};
 
 pub fn socket_path() -> PathBuf {
     dirs_next::runtime_dir()
@@ -10,12 +11,12 @@ pub fn socket_path() -> PathBuf {
         .join("v5d.sock")
 }
 
-pub fn connect_to_socket() -> io::Result<Socket> {
+pub async fn connect_to_socket() -> io::Result<UnixStream> {
     let path = socket_path();
     debug!("Connecting to UNIX socket at {:?}", path);
 
-    let socket = Socket::new(Domain::UNIX, Type::STREAM, None).unwrap();
-    socket.connect(&SockAddr::unix(&path)?)?;
+    let socket = UnixStream::connect(&path).await?;
+    socket.ready(Interest::READABLE | Interest::WRITABLE).await?;
 
     info!("Connected to UNIX socket at {:?}", path);
     Ok(socket)
--- packages/v5d/src/main.rs
+++ packages/v5d/src/main.rs
@@ -2,17 +2,16 @@ use std::io::Write;
 use std::io::{self, Read};
 
 use log::{debug, error, info};
-use socket2::{Domain, SockAddr, Socket, Type};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::UnixListener;
 use v5d_interface::{socket_path, DaemonResponse};
 use v5d_interface::DaemonCommand;
 
 /// Creates a UNIX socket to communicate with the V5 Daemon
-pub fn setup_socket() -> io::Result<Socket> {
+pub fn setup_socket() -> io::Result<UnixListener> {
     let path = socket_path();
 
-    let socket = Socket::new(Domain::UNIX, Type::STREAM, None).unwrap();
-    socket.bind(&SockAddr::unix(&path)?)?;
-    socket.listen(128)?;
+    let socket = UnixListener::bind(&path)?;
 
     info!("UNIX socket created and bound to {:?}", path);
     info!("Listening for incoming connections...");
@@ -30,7 +29,7 @@ fn on_shutdown() {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     simplelog::TermLogger::init(
-        log::LevelFilter::Debug,
+        log::LevelFilter::Trace,
         Default::default(),
         simplelog::TerminalMode::Mixed,
         simplelog::ColorChoice::Auto,
@@ -39,19 +38,21 @@ async fn main() -> anyhow::Result<()> {
 
     let socket = setup_socket()?;
     loop {
-        match socket.accept() {
+        match socket.accept().await {
             Ok((mut stream, _addr)) => {
+                stream.readable().await?;
                 info!("Accepted connection from client");
                 let mut content = String::new();
-                stream.read_to_string(&mut content)?;
+                stream.read_to_string(&mut content).await?;
                 
                 let command: DaemonCommand = serde_json::from_str(&content)?;
                 debug!("Received command: {:?}", command);
+                
+                stream.writable().await?;
+                let DaemonCommand::Test(string) = command;
 
-                // let DaemonCommand::Test(string) = command;
-
-                // let response = DaemonResponse::Test(string);
-                // stream.write_all(serde_json::to_string(&response)?.as_bytes())?;
+                let response = DaemonResponse::Test(string);
+                stream.write_all(serde_json::to_string(&response)?.as_bytes()).await?;
             }
             Err(e) => {
                 error!("Failed to accept connection: {}", e);
