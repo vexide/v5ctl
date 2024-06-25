2fa8d14d54381993cb67b5caaae5c161668b3346
Gavin-Niederman <gavinniederman@gmail.com>
2024-06-25 11:35:11 -0700

    feat: cleanly exit v5d

--- packages/v5d/src/main.rs
+++ packages/v5d/src/main.rs
@@ -4,7 +4,6 @@ use log::{debug, error, info};
 use socket2::{Domain, SockAddr, Socket, Type};
 use v5d_interface::socket_path;
 
-
 /// Creates a UNIX socket to communicate with the V5 Daemon
 pub fn setup_socket() -> io::Result<Socket> {
     let path = socket_path();
@@ -12,16 +11,30 @@ pub fn setup_socket() -> io::Result<Socket> {
 
     let socket = Socket::new(Domain::UNIX, Type::STREAM, None).unwrap();
     socket.bind(&SockAddr::unix(&path)?)?;
+    socket.listen(128)?;
 
     info!("UNIX socket created and bound to {:?}", path);
     info!("Listening for incoming connections...");
     Ok(socket)
 }
 
+fn on_shutdown() {
+    info!("Shutting down...");
+    // Clean up the socket file
+    let _ = std::fs::remove_file(socket_path());
+    info!("Shutdown complete!");
+    std::process::exit(0);
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    simplelog::TermLogger::init(
+        log::LevelFilter::Debug,
+        Default::default(),
+        simplelog::TerminalMode::Mixed,
+        simplelog::ColorChoice::Auto,
+    )?;
+    ctrlc::set_handler(on_shutdown)?;
 
     let socket = setup_socket()?;
     loop {
